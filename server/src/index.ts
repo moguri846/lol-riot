14c6568f6260319eab4cb45962dd2a8616a2cfd5
moguri846 <dkffkekahsl1@naver.com>
2022-01-06 19:14:39 +0900

    [BE] feat !: cors 설정
    cors 설정

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,22 @@
 import express from "express";
+import cors from "cors";
 const app: express.Application = express();
 const port: number = 5000;
+
+// 이 주소만 cors 허용
+const whitelist = ["http://localhost:3000"];
+
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not Allowed Origin!"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.listen(port, () => {
   console.log(`connected ${port}`);

99695412e2a4225fa626c60179775db706be9cbe
moguri846 <dkffkekahsl1@naver.com>
2022-01-08 23:42:36 +0900

    [BE] chore !: searchSummoner API response 수정
    searchSummoner API response 수정

--- server/src/routes/riot.ts
+++ server/src/routes/riot.ts
@@ -18,6 +18,8 @@ router.post("/searchSummoner", async (req: Request, res: Response) => {
     // matchId로 match 데이터 받아온 후 matchArr에 push
     await Promise.all(
       matchIds.data.map(async (matchId: string) => {
+        let redTeamPlayers: any[] = [];
+        let blueTeamPlayers: any[] = [];
         const match: AxiosResponse<Match> = await getMatchInfo(matchId);
 
         // 내 index 확인
@@ -28,12 +30,58 @@ router.post("/searchSummoner", async (req: Request, res: Response) => {
           }
         }
 
-        matchArr.push({ ...match.data, myIndex });
+        // 팀 나누기
+        for (let i = 0; i < match.data.info.participants.length; i++) {
+          let appendValues: any = {
+            championName: match.data.info.participants[i].championName,
+            summonerName: match.data.info.participants[i].summonerName,
+            puuid: match.data.info.participants[i].puuid,
+          };
+
+          if (i < 5) {
+            blueTeamPlayers.push(appendValues);
+          } else {
+            redTeamPlayers.push(appendValues);
+          }
+        }
+
+        const appendValues = {
+          gameCreation: match.data.info.gameCreation,
+          gameEndTimestamp: match.data.info.gameEndTimestamp ? match.data.info.gameEndTimestamp : null,
+          gameStartTimestamp: match.data.info.gameStartTimestamp,
+          gameId: match.data.info.gameId,
+          gameMode: match.data.info.gameMode,
+          player: {
+            summonerName: match.data.info.participants[myIndex].summonerName,
+            championName: match.data.info.participants[myIndex].championName,
+            kills: match.data.info.participants[myIndex].kills,
+            deaths: match.data.info.participants[myIndex].deaths,
+            assists: match.data.info.participants[myIndex].assists,
+            champLevel: match.data.info.participants[myIndex].champLevel,
+            cs:
+              match.data.info.participants[myIndex].totalMinionsKilled +
+              match.data.info.participants[myIndex].neutralMinionsKilled,
+            items: [
+              match.data.info.participants[myIndex].item0,
+              match.data.info.participants[myIndex].item1,
+              match.data.info.participants[myIndex].item2,
+              match.data.info.participants[myIndex].item3,
+              match.data.info.participants[myIndex].item6,
+              match.data.info.participants[myIndex].item4,
+              match.data.info.participants[myIndex].item5,
+            ],
+            win: match.data.info.participants[myIndex].win,
+          },
+          redTeamPlayers,
+          blueTeamPlayers,
+        };
+
+        matchArr.push({ ...appendValues });
       })
     );
 
     // gameCreation기준 내림차순 정렬
-    matchArr.sort((a, b) => b.info.gameCreation - a.info.gameCreation);
+    matchArr.sort((a, b) => b.gameCreation - a.gameCreation);
 
     res.json({ success: true, summoner: matchArr });
   } catch (err: any) {

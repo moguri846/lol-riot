f5cd4e27f30714d991bee5e9eecfa6ee46c624b0
moguri846 <dkffkekahsl1@naver.com>
2022-01-23 14:05:31 +0900

    [BE] fix !: undefined가 200으로 나오는 에러 해결
    undefined가 200으로 나오는 에러 해결

--- server/src/common/ResSuccessOrFalse.function.ts
+++ server/src/common/ResSuccessOrFalse.function.ts
@@ -1,5 +1,5 @@
 import { ResFuncType } from "./interface/ResSuccessOrFail.interface";
 
-export const resFunc = ({ res, status = 200, success, data, errMessage }: ResFuncType) => {
+export const resFunc = ({ res, status, success, data, errMessage }: ResFuncType) => {
   return res.status(status || 500).json({ success, data, errMessage });
 };

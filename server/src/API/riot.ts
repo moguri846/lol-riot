4189e5dc7dcd2d552a63840d2ea5856ef640db2e
moguri846 <dkffkekahsl1@naver.com>
2022-01-25 22:23:11 +0900

    [BE] refactor !: api url 중복 제거
    api url 중복 제거

--- server/src/API/riot.ts
+++ server/src/API/riot.ts
@@ -1,23 +1,28 @@
 import Send from "./interceptor";
 
+enum RIOT_URL {
+  KR = "https://kr.api.riotgames.com/lol",
+  ASIA = "https://asia.api.riotgames.com/lol",
+}
+
 const getSummonerInfo = (summoner: string) => {
   return Send({
     method: "GET",
-    url: `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURIComponent(summoner)}`,
+    url: `${RIOT_URL.KR}/summoner/v4/summoners/by-name/${encodeURIComponent(summoner)}`,
   });
 };
 
 const getMatchIds = (puuid: string) => {
   return Send({
     method: "GET",
-    url: `https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=1`,
+    url: `${RIOT_URL.ASIA}/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=2`,
   });
 };
 
 const getMatchInfo = (matchId: string) => {
   return Send({
     method: "GET",
-    url: `https://asia.api.riotgames.com/lol/match/v5/matches/${matchId}`,
+    url: `${RIOT_URL.ASIA}/match/v5/matches/${matchId}`,
   });
 };
 

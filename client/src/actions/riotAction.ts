b7a42610052375c4d2ac5be49534247e3362412c
moguri846 <dkffkekahsl1@naver.com>
2022-01-29 01:31:01 +0900

    [FE] chore !: type 변수로 변경
    type 변수로 변경

--- client/src/actions/riotAction.ts
+++ client/src/actions/riotAction.ts
@@ -2,14 +2,14 @@ import { AxiosResponse } from "axios";
 import { Dispatch } from "redux";
 import { getSummonerMatchList, getMatchDetailInfo } from "../API/riot";
 import { MatchDispatchType, MatchType, MatchDetailType } from "./interface/riotAction.interface";
-import { SUCCESS_MATCH, FAIL, SUCCESS_DETAIL } from "./type";
+import { SUCCESS_MATCH_SUMMARY, FAIL, SUCCESS_MATCH_SUMMARY_DETAIL } from "./type";
 
 const summonerMatchList = (summonerName: string) => async (dispatch: Dispatch<MatchDispatchType<MatchType>>) => {
   try {
     const { data }: AxiosResponse<{ success: boolean; summoner: MatchType }> = await getSummonerMatchList(summonerName);
 
     dispatch({
-      type: SUCCESS_MATCH,
+      type: SUCCESS_MATCH_SUMMARY,
       payload: data.summoner,
     });
   } catch (err: any) {
@@ -25,7 +25,7 @@ const matchDetailInfo = (gameId: number) => async (dispatch: Dispatch<MatchDispa
     const { data }: AxiosResponse<{ success: boolean; match: MatchDetailType }> = await getMatchDetailInfo(gameId);
 
     dispatch({
-      type: SUCCESS_DETAIL,
+      type: SUCCESS_MATCH_SUMMARY_DETAIL,
       payload: data.match,
     });
   } catch (err: any) {

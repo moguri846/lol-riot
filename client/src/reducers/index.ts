2f1ea0c960e7dffee8c80b394d49d625d95b9687
moguri846 <dkffkekahsl1@naver.com>
2022-01-30 22:31:08 +0900

    [FE] chore !: 일별 CLASSIC게임 승률 표시 reducer 추가
    일별 CLASSIC게임 승률 표시 reducer 추가

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import matchSummaryReducer from "./matchSummaryReducer";
+import jandiReducer from "./jandiReducer";
 
 const rootReducer = combineReducers({
   matchSummaryReducer,
+  jandiReducer,
 });
 
 export type RootReducer = ReturnType<typeof rootReducer>;
---
+++ client/src/reducers/jandiReducer.ts
@@ -0,0 +1,27 @@
+import { Jandi } from "../actions/interface/jandi.interface";
+import { JANDI } from "../actions/type";
+
+const inititalState: Jandi[] = [
+  {
+    date: "2022-01-11",
+    win: 0,
+    lose: 0,
+    count: 0,
+  },
+];
+
+type ActionType = { type: typeof JANDI; payload: Jandi[] };
+
+const reducer = (state = inititalState, action: ActionType) => {
+  switch (action.type) {
+    case JANDI: {
+      // TODO: return 값 수정
+      return [...action.payload];
+    }
+    default: {
+      return [...state];
+    }
+  }
+};
+
+export default reducer;

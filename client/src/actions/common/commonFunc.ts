e8335d0c04e7a205b75b99c314e006e21936bebb
moguri846 <dkffkekahsl1@naver.com>
2022-01-30 22:32:41 +0900

    [FE] chore !: summonerMatchList 코드 수정

--- client/src/actions/common/commonFunc.ts
+++ client/src/actions/common/commonFunc.ts
@@ -4,23 +4,32 @@ import { getSummonerMatchList, getMatchDetailInfo } from "../../API/riot";
 import { MatchListFilterType } from "./interface/commonFunc.interface";
 import { MatchSummaryType, MatchSummaryDetailType } from "../interface/matchSummary.interface";
 import { SuccessMatchList, SuccessMatchDetailInfo } from "./interface/commonFunc.interface";
-import { MATCH_SUMMARY_DETAIL, FAIL, MATCH_SUMMARY } from "../type";
+import { MATCH_SUMMARY_DETAIL, FAIL, MATCH_SUMMARY, JANDI } from "../type";
 import { ComparingWithEnemyType } from "../interface/comparingWithEnemy.interface";
+import { Jandi } from "../interface/jandi.interface";
 
 const summonerMatchList = (summonerName: string, type: MatchListFilterType) => {
   type T = typeof type extends typeof MATCH_SUMMARY ? MatchSummaryType[] : ComparingWithEnemyType[];
-  return async (dispatch: Dispatch<SuccessMatchList<T>>) => {
+  return async (dispatch: Dispatch<SuccessMatchList<any>>) => {
     try {
       const {
         data,
       }: AxiosResponse<{
         success: boolean;
-        data: T;
+        data: {
+          matchArr: T;
+          jandi: Jandi[];
+        };
       }> = await getSummonerMatchList(summonerName, type);
 
       dispatch({
         type,
-        payload: data.data,
+        payload: data.data.matchArr,
+      });
+
+      dispatch({
+        type: JANDI,
+        payload: data.data.jandi,
       });
     } catch (err: any) {
       dispatch({

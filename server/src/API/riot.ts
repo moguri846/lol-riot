ca8d73563949fd290eac7594865d5eeef74909d8
moguri846 <dkffkekahsl1@naver.com>
2022-02-06 20:55:53 +0900

    [BE] refactor !: api url 공통 코드 수정
    api url 공통 코드 수정

--- server/src/API/riot.ts
+++ server/src/API/riot.ts
@@ -1,42 +1,46 @@
 import Send from "./interceptor";
 
-enum RIOT_URL {
-  KR = "https://kr.api.riotgames.com/lol",
-  ASIA = "https://asia.api.riotgames.com/lol",
+enum REGION {
+  KR = "kr",
+  ASIA = "asia",
 }
+
+const commonUrl = (region: string) => {
+  return `https://${region}.api.riotgames.com/lol`;
+};
 
 const getSummonerInfo = (summoner: string) => {
   return Send({
     method: "GET",
-    url: `${RIOT_URL.KR}/summoner/v4/summoners/by-name/${encodeURIComponent(summoner)}`,
+    url: `${commonUrl(REGION.KR)}/summoner/v4/summoners/by-name/${encodeURIComponent(summoner)}`,
   });
 };
 
 const getSummonerDetailInfo = (encryptedSummonerId: string) => {
   return Send({
     method: "GET",
-    url: `${RIOT_URL.KR}/league/v4/entries/by-summoner/${encryptedSummonerId}`,
+    url: `${commonUrl(REGION.KR)}/league/v4/entries/by-summoner/${encryptedSummonerId}`,
   });
 };
 
 const getMatchIds = (puuid: string) => {
   return Send({
     method: "GET",
-    url: `${RIOT_URL.ASIA}/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=2`,
+    url: `${commonUrl(REGION.ASIA)}/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=2`,
   });
 };
 
 const getMatchInfo = (matchId: string) => {
   return Send({
     method: "GET",
-    url: `${RIOT_URL.ASIA}/match/v5/matches/${matchId}`,
+    url: `${commonUrl(REGION.ASIA)}/match/v5/matches/${matchId}`,
   });
 };
 
 const getMatchTimeLine = (matchId: string) => {
   return Send({
     method: "GET",
-    url: `${RIOT_URL.ASIA}/match/v5/matches/${matchId}/timeline`,
+    url: `${commonUrl(REGION.ASIA)}/match/v5/matches/${matchId}/timeline`,
   });
 };
 

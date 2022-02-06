b36fee4828c9f63de1f429346aa86a8ef517d1a5
moguri846 <dkffkekahsl1@naver.com>
2022-02-06 14:46:25 +0900

    [FE] refactor !: matchSummaryReducer MATCH_SUMMARY 삭제
    matchSummaryReducer MATCH_SUMMARY 삭제

--- client/src/reducers/matchSummaryReducer.ts
+++ client/src/reducers/matchSummaryReducer.ts
@@ -1,75 +1,53 @@
-import { MatchSummaryDetailType, MatchSummaryType } from "../actions/interface/matchSummary.interface";
-import { MATCH_SUMMARY, MATCH_SUMMARY_DETAIL, FAIL } from "../actions/type";
+import { ComparingWithEnemyType, IComparingWithEnemyDetail } from "../actions/interface/comparingWithEnemy.interface";
+import { FAIL, COMPARING_WITH_ENEMY, COMPARING_WITH_ENEMY_DETAIL } from "../actions/type";
 
-const initialState: MatchSummaryType[] = [
+const playerInitialValue = {
+  goldEarned: 0,
+  physicalDamageDealtToChampions: 0,
+  totalDamageDealt: 0,
+  index: 0,
+};
+
+const initialState: ComparingWithEnemyType[] = [
   {
-    gameCreation: 0,
-    gameEndTimestamp: 0,
     gameId: 0,
     gameMode: "",
-    gameStartTimestamp: 0,
-    player: {
-      assists: 0,
-      champLevel: 0,
-      championName: "",
-      cs: 0,
-      deaths: 0,
-      items: [],
-      kills: 0,
-      perks: {
-        statPerks: {
-          defense: 0,
-          flex: 0,
-          offense: 0,
-        },
-        styles: {
-          description: "",
-          selections: [
-            {
-              perk: 0,
-              var1: 0,
-              var2: 0,
-              var3: 0,
-            },
-          ],
-          style: 0,
-        },
-      },
-      spells: [],
-      summonerName: "",
-      win: false,
-    },
-    players: [
-      {
-        championName: "",
-        puuid: "",
-        summonerName: "",
-      },
-    ],
+    gameCreation: 0,
+    player: playerInitialValue,
+    enemy: playerInitialValue,
     detail: null,
   },
 ];
 
 type ActionType =
-  | { type: typeof MATCH_SUMMARY; payload: MatchSummaryType }
-  | { type: typeof MATCH_SUMMARY_DETAIL; payload: MatchSummaryDetailType }
+  | { type: typeof COMPARING_WITH_ENEMY; payload: ComparingWithEnemyType[] }
+  | { type: typeof COMPARING_WITH_ENEMY_DETAIL; payload: IComparingWithEnemyDetail }
   | { type: typeof FAIL; payload: { success: boolean; errMessage: string } };
 
-const reducer = (state = initialState, action: ActionType) => {
+const reducer = (state = [...initialState], action: ActionType) => {
   switch (action.type) {
-    case MATCH_SUMMARY:
-      return { ...state, ...action.payload };
-    case MATCH_SUMMARY_DETAIL: {
-      Object.values(state).filter((match) => {
+    case COMPARING_WITH_ENEMY:
+      type CWET = [string, ComparingWithEnemyType];
+      const arr: ComparingWithEnemyType[] = [];
+      const sparedValue = { ...state, ...action.payload };
+      Object.entries(sparedValue).map((value) => {
+        const [k, v] = value as CWET;
+
+        arr.push(v);
+      });
+
+      return [...arr];
+    case COMPARING_WITH_ENEMY_DETAIL: {
+      state.filter((match) => {
         if (match.gameId === action.payload.gameId) {
           match.detail = action.payload;
         }
       });
-      return { ...state };
+      return [...state];
     }
 
     default:
-      return { ...state };
+      return [...state];
   }
 };
 

80eec28dccc87fb1f0da166790b4be2ece5e0b02
moguri846 <dkffkekahsl1@naver.com>
2022-02-10 23:35:13 +0900

    [FE] feat !: Timeline graph 추가
    Timeline graph 추가

---
+++ client/src/components/LineGraph/LineGraph.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+import { CategoryScale } from "chart.js";
+import Chart from "chart.js/auto";
+import { IDetail } from "../../actions/interface/matchSummary.interface";
+
+Chart.register(CategoryScale);
+
+interface Props {
+  timeline: {
+    player: IDetail;
+    enemy: IDetail;
+  }[];
+  isOpen: boolean;
+}
+
+const LineGraph = ({ timeline, isOpen }: Props) => {
+  const labels = timeline.map((line, index) => `${index}분`);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "플레이어",
+        data: timeline.map((line) => line.player.totalGold),
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+      {
+        label: "상대",
+        data: timeline.map((line) => line.enemy.totalGold),
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+  };
+
+  return <>{isOpen ? <Line data={data} options={options} /> : <div>loading</div>}</>;
+};
+
+export default LineGraph;

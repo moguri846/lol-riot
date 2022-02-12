824303bc4f30c6bea31c53dfec5cae3bdbea026f
moguri846 <dkffkekahsl1@naver.com>
2022-02-12 23:46:17 +0900

    [FE] refactor !: LineGraph 코드 수정
    LineGraph 코드 수정

--- client/src/components/LineGraph/LineGraph.tsx
+++ client/src/components/LineGraph/LineGraph.tsx
@@ -3,28 +3,31 @@ import { Line } from "react-chartjs-2";
 import { CategoryScale } from "chart.js";
 import Chart from "chart.js/auto";
 import { ITimeLine } from "../../actions/interface/matchSummary.interface";
+import { TOTAL_GOLD, XP } from "./constant/LineGraph.constant";
 
 Chart.register(CategoryScale);
 
+type Type = typeof TOTAL_GOLD | typeof XP;
+
 interface Props {
-  timeline: ITimeLine[];
-  isOpen: boolean;
+  timeline: ITimeLine[] | undefined;
+  type: Type;
 }
 
-const LineGraph = ({ timeline, isOpen }: Props) => {
-  const labels = timeline.map((line, index) => `${index}분`);
+const LineGraph = ({ timeline, type }: Props) => {
+  const labels = timeline?.map((line, index) => `${index}분`);
 
   const data = {
     labels,
     datasets: [
       {
         label: "플레이어",
-        data: timeline.map((line) => line.player.totalGold),
+        data: timeline?.map((line) => line.player[type]),
         backgroundColor: "rgba(53, 162, 235, 0.5)",
       },
       {
         label: "상대",
-        data: timeline.map((line) => line.enemy.totalGold),
+        data: timeline?.map((line) => line.enemy[type]),
         backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
     ],
@@ -33,8 +36,7 @@ const LineGraph = ({ timeline, isOpen }: Props) => {
   const options = {
     responsive: true,
   };
-
-  return <>{isOpen ? <Line data={data} options={options} /> : <div>loading</div>}</>;
+  return <>{timeline ? <Line data={data} options={options} /> : <>Spinner</>}</>;
 };
 
 export default LineGraph;
---
+++ client/src/components/LineGraph/constant/LineGraph.constant.ts
@@ -0,0 +1,4 @@
+const TOTAL_GOLD = "totalGold";
+const XP = "xp";
+
+export { TOTAL_GOLD, XP };

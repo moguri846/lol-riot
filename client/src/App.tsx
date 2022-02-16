38ab3585917167715534db010bcf186dc630b856
moguri846 <dkffkekahsl1@naver.com>
2022-02-16 22:11:21 +0900

    [FE] chore !: App.tsx layout 잡기
    App.tsx layout 잡기

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import Router from "./routes/Router";
+import GlobalStyle from "./styles/GlobalStyle";
 
-function App() {
+const Wrap = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  // width: 800px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Wrap>
+        <Container>
+          <Router />
+        </Container>
+      </Wrap>
+    </>
   );
-}
+};
 
 export default App;

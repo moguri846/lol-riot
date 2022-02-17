ad81c9711e005d32f76b138ab5723f304673cbda
moguri846 <dkffkekahsl1@naver.com>
2022-02-17 18:29:53 +0900

    [FE] chore !: input, button padding 2% => 6px
    input, button padding 2% => 6px

--- client/src/components/Atoms/Button/style.ts
+++ client/src/components/Atoms/Button/style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Button = styled.button`
   width: 100%;
   border: 1px solid black;
-  padding: 2%;
+  padding: 6px;
 
   background-color: ${({ disabled }) => (disabled ? "lightgray" : "white")};
 `;
--- client/src/components/Atoms/Input/style.ts
+++ client/src/components/Atoms/Input/style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Input = styled.input`
   width: 100%;
   border: 1px solid black;
-  padding: 2%;
+  padding: 6px;
   outline: none;
   background-color: ${({ disabled }) => (disabled ? "lightgray" : "white")};
 `;

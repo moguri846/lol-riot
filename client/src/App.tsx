0704cafd5c12b7b6aa84cef365daaefc68d5fabe
moguri846 <dkffkekahsl1@naver.com>
2022-02-23 16:28:42 +0900

    [FE] feat !: OAuth logout
    OAuth logout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { oAuthLogout } from "./API/oauth";
 import Header from "./components/Organisms/Header/Header";
 import Router from "./routes/Router";
 import GlobalStyle from "./styles/GlobalStyle";
@@ -21,12 +22,32 @@ const Main = styled.main`
 `;
 
 const App = () => {
+  const logoutOAuth = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    try {
+      const type = localStorage.getItem("OAUTH_TYPE") as string;
+
+      const { data } = await oAuthLogout(type);
+
+      if (data.success) {
+        localStorage.clear();
+      }
+    } catch (err: any) {
+      alert(`무언가 이상해요! ${err.message}`);
+    }
+  };
+
+  const goToOAuthLink = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const link = e.currentTarget.dataset.link as string;
+    window.location.href = link;
+  };
+
   return (
     <>
       <GlobalStyle />
       <Header />
       <Wrap>
         <Main>
+          <button onClick={logoutOAuth}>로그아웃</button>
           <Router />
         </Main>
       </Wrap>

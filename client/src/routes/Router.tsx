1f20897f8e37f5232562e9d9f9d36d412f0a5268
moguri846 <dkffkekahsl1@naver.com>
2022-02-23 16:24:09 +0900

    [FE] feat !: route 추가
    route 추가

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -3,12 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import React from "react";
 import IndexPage from "../pages/IndexPage/IndexPage";
 import SummonerPage from "../pages/SummonerPage/SummonerPage";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import OAuth from "../components/OAuth";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<IndexPage />} />
       <Route path="/summoner=:summonerName" element={<SummonerPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/oauth/:type" element={<OAuth />} />
     </Routes>
   );
 };

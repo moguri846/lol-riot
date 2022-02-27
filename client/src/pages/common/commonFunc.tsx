d7df543a45ad067936cbca9b712e03dfc0e2e981
moguri846 <dkffkekahsl1@naver.com>
2022-02-27 18:16:45 +0900

    [FE] chore !: data dragon version 업데이트
    data dragon version 업데이트

--- client/src/pages/common/commonFunc.tsx
+++ client/src/pages/common/commonFunc.tsx
@@ -1,6 +1,6 @@
 const getDataDragonImg = (key: string, value: string | number) => {
   return (
-    <img key={value} src={`https://ddragon.leagueoflegends.com/cdn/12.3.1/img/${key}/${value}.png`} alt={`${value}`} />
+    <img key={value} src={`https://ddragon.leagueoflegends.com/cdn/12.4.1/img/${key}/${value}.png`} alt={`${value}`} />
   );
 };
 

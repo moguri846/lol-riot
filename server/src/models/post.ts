d8c2561b0732d36bbb7d1591ecd1f6f24bc5b843
moguri846 <dkffkekahsl1@naver.com>
2022-03-01 22:45:35 +0900

    [BE] feat !: Post Schema 추가
    Post Schema 추가

---
+++ server/src/models/post.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new mongoose.Schema({
+  writer: {
+    type: String,
+  },
+  title: {
+    type: String,
+  },
+  content: {
+    type: String,
+  },
+  views: {
+    type: Number,
+  },
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+export { Post };

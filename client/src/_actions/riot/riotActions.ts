51a040ddffde944539a759ef4b65c161b4b00af4
moguri846 <dkffkekahsl1@naver.com>
2022-03-06 19:46:44 +0900

    [FE] refactor !: summonerMatchList action => summonerInfo, matchInfo으로 쪼갬
    summonerMatchList action => summonerInfo, matchInfo으로 쪼갬

--- client/src/_actions/riot/riotActions.ts
+++ client/src/_actions/riot/riotActions.ts
@@ -1,6 +1,6 @@
 import { AxiosResponse } from "axios";
 import { Dispatch } from "redux";
-import { getSummonerMatchList, getMatchDetailInfo, loadSummonerMatchInfo } from "../../API/riot";
+import { loadSummonerInfo, loadMatchInfo, loadMatchDetailInfo } from "../../API/riot";
 import { FAIL } from "../common/constant/common.constant";
 import { IMatchDetailResponse, IMatchInfo, ISummoner, MatchListFilterType } from "./interface/dispatch.interface";
 import { SuccessMatchList, SuccessMatchDetailInfo } from "./interface/dispatch.interface";
@@ -12,19 +12,14 @@ import {
   LINE_WIN_OR_LOSE,
 } from "./constant/riot.constant";
 import { IComparingWithEnemyDetail, IMatchSummaryDetailParameter } from "./interface/matchSummary.interface";
+import { SummonerType } from "./interface/summoner.interface";
 
-const summonerMatchList = (summonerName: string, type: MatchListFilterType) => {
+const summonerInfo = (summonerName: string, type: MatchListFilterType) => {
   return async (dispatch: Dispatch<SuccessMatchList<any>>) => {
     try {
       const {
         data: { data: summoner },
-      }: AxiosResponse<ISummoner> = await getSummonerMatchList(summonerName, type);
-
-      const {
-        data: {
-          data: { matchArr, jandi, lineWinOrLose },
-        },
-      }: AxiosResponse<IMatchInfo> = await loadSummonerMatchInfo(summoner.puuid);
+      }: AxiosResponse<ISummoner> = await loadSummonerInfo(summonerName, type);
 
       // 유저 정보
       dispatch({
@@ -32,23 +27,7 @@ const summonerMatchList = (summonerName: string, type: MatchListFilterType) => {
         payload: summoner,
       });
 
-      // 게임 리스트
-      dispatch({
-        type: COMPARING_WITH_ENEMY,
-        payload: matchArr,
-      });
-
-      // 잔디
-      dispatch({
-        type: JANDI,
-        payload: jandi,
-      });
-
-      // 라인 별 승패
-      dispatch({
-        type: LINE_WIN_OR_LOSE,
-        payload: lineWinOrLose,
-      });
+      return summoner;
     } catch (err: any) {
       const errMessage = err?.response?.data?.errMessage || err.message;
       dispatch({
@@ -59,11 +38,45 @@ const summonerMatchList = (summonerName: string, type: MatchListFilterType) => {
   };
 };
 
+const matchInfo = (puuid: string) => async (dispatch: Dispatch<any>) => {
+  try {
+    const {
+      data: {
+        data: { matchArr, jandi, lineWinOrLose },
+      },
+    }: AxiosResponse<IMatchInfo> = await loadMatchInfo(puuid);
+
+    // 게임 리스트
+    dispatch({
+      type: COMPARING_WITH_ENEMY,
+      payload: matchArr,
+    });
+
+    // 잔디
+    dispatch({
+      type: JANDI,
+      payload: jandi,
+    });
+
+    // 라인 별 승패
+    dispatch({
+      type: LINE_WIN_OR_LOSE,
+      payload: lineWinOrLose,
+    });
+  } catch (err: any) {
+    const errMessage = err?.response?.data?.errMessage || err.message;
+    dispatch({
+      type: FAIL,
+      payload: { errMessage },
+    });
+  }
+};
+
 const matchDetailInfo =
   (parameterObj: IMatchSummaryDetailParameter) =>
   async (dispatch: Dispatch<SuccessMatchDetailInfo<IComparingWithEnemyDetail[]>>) => {
     try {
-      const { data }: AxiosResponse<IMatchDetailResponse> = await getMatchDetailInfo(parameterObj);
+      const { data }: AxiosResponse<IMatchDetailResponse> = await loadMatchDetailInfo(parameterObj);
 
       dispatch({
         type: COMPARING_WITH_ENEMY_DETAIL,
@@ -78,4 +91,4 @@ const matchDetailInfo =
     }
   };
 
-export { summonerMatchList, matchDetailInfo };
+export { summonerInfo, matchInfo, matchDetailInfo };

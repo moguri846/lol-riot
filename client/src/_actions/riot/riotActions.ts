bb4bd3008951b41382849ca525857d26c128b4d8
moguri846 <dkffkekahsl1@naver.com>
2022-03-18 19:32:51 +0900

    [FE] feat !: 게임 중인지 확인 기능 redux 추가
    게임 중인지 확인 기능 redux 추가

--- client/src/_actions/riot/riotActions.ts
+++ client/src/_actions/riot/riotActions.ts
@@ -1,6 +1,6 @@
 import { AxiosResponse } from "axios";
 import { Dispatch } from "redux";
-import { loadSummonerInfo, loadMatchInfo, loadMatchDetailInfo } from "../../API/riot";
+import { loadSummonerInfo, loadMatchInfo, loadMatchDetailInfo, loadSpectatorInfo } from "../../API/riot";
 import { FAIL } from "../common/constant/common.constant";
 import { IMatchDetailResponse, IMatchInfo, ISummoner, MatchListFilterType } from "./interface/dispatch.interface";
 import { SuccessMatchList, SuccessMatchDetailInfo } from "./interface/dispatch.interface";
@@ -10,8 +10,10 @@ import {
   COMPARING_WITH_ENEMY_DETAIL,
   JANDI,
   LINE_WIN_OR_LOSE,
+  SPECTATOR,
 } from "./constant/riot.constant";
 import { IComparingWithEnemyDetail, IMatchSummaryDetailParameter } from "./interface/matchSummary.interface";
+import { ISpectator } from "./interface/spectator.interface";
 
 const summonerInfo =
   (summonerName: string, type: MatchListFilterType) => async (dispatch: Dispatch<SuccessMatchList<any>>) => {
@@ -76,6 +78,28 @@ const matchInfo = (puuid: string) => async (dispatch: Dispatch<any>) => {
   }
 };
 
+const spectatorInfo = (encryptedSummonerId: string) => async (dispatch: Dispatch<any>) => {
+  try {
+    const {
+      data: { data: spectator },
+    }: AxiosResponse<{ data: ISpectator }> = await loadSpectatorInfo(encryptedSummonerId);
+
+    dispatch({
+      type: SPECTATOR,
+      payload: spectator,
+    });
+  } catch (err: any) {
+    const errMessage = err?.response?.data?.errMessage || err.message;
+
+    dispatch({
+      type: FAIL,
+      payload: { errMessage },
+    });
+
+    throw errMessage;
+  }
+};
+
 const matchDetailInfo =
   (parameterObj: IMatchSummaryDetailParameter) =>
   async (dispatch: Dispatch<SuccessMatchDetailInfo<IComparingWithEnemyDetail[]>>) => {
@@ -98,4 +122,4 @@ const matchDetailInfo =
     }
   };
 
-export { summonerInfo, matchInfo, matchDetailInfo };
+export { summonerInfo, matchInfo, spectatorInfo, matchDetailInfo };
--- client/src/_reducers/riot/riotRootReducer.ts
+++ client/src/_reducers/riot/riotRootReducer.ts
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
 
 import summoner from "./summonerReducer";
+import spectator from "./spectatorReducer";
 import matchSummary from "./matchSummaryReducer";
 import jandi from "./jandiReducer";
 import lineWinOrLose from "./lineWinOrLoseReducer";
 
 const riotRootReducer = combineReducers({
   summoner,
+  spectator,
   matchSummary,
   jandi,
   lineWinOrLose,

5bbc5492ee7ae46894306dc3954189fc7dd79119
moguri846 <dkffkekahsl1@naver.com>
2022-03-18 19:34:40 +0900

    [BE] feat !: 게임 중인지 확인 기능 interface 추가
    게임 중인지 확인 기능 interface 추가

--- server/src/routes/riot/interface/riot.interface.ts
+++ server/src/routes/riot/interface/riot.interface.ts
@@ -265,3 +265,44 @@ export interface Jandi {
   lose: number;
   count: number;
 }
+
+export interface ISpectator {
+  bannedChampions: {
+    championId: number;
+    championName: string;
+    pickTurn: number;
+    teamId: number;
+  }[];
+  gameId: number;
+  gameLength: number;
+  gameMode: string;
+  gameQueueConfigId: number;
+  gameStartTime: number;
+  gameType: string;
+  mapId: number;
+  observers: {
+    encryptionKey: string;
+  };
+  participants: {
+    bot: boolean;
+    championId: number;
+    championName: string;
+    spells: [string, string];
+    gameCustomizationObjects: {
+      category?: string;
+      content?: string;
+    }[];
+    perks: {
+      perkIds: number[];
+      perkStyle: number;
+      perkSubStyle: number;
+    };
+    profileIconId: number;
+    spell1Id: number;
+    spell2Id: number;
+    summonerId: string;
+    summonerName: string;
+    teamId: number;
+  }[];
+  platformId: string;
+}

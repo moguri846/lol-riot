4c4d1a8abf16dbe2b542dfbec42e0ac7678b7ce0
moguri846 <dkffkekahsl1@naver.com>
2022-03-19 19:32:26 +0900

    [FE] refactor !: getDataDragonImg key parameter 타입 수정
    getDataDragonImg key parameter 타입 수정

--- client/src/pages/common/commonFunc.tsx
+++ client/src/pages/common/commonFunc.tsx
@@ -1,4 +1,4 @@
-const getDataDragonImg = (key: string, value: string | number) => {
+const getDataDragonImg = (key: "champion" | "spell" | "item" | "profileicon", value: string | number) => {
   return (
     <img key={value} src={`https://ddragon.leagueoflegends.com/cdn/12.4.1/img/${key}/${value}.png`} alt={`${value}`} />
   );

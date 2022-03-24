37be658b8898798d4261380203ab4d5a2022a419
moguri846 <dkffkekahsl1@naver.com>
2022-03-24 21:03:41 +0900

    [BE] refactor !: riot API swagger response 수정
    riot API swagger response 수정

--- server/src/routes/riot/riot.ts
+++ server/src/routes/riot/riot.ts
@@ -20,32 +20,51 @@ const router = Router();
 
 /**
  * @swagger
- * /api/riot/searchSummoner:
+ * /api/riot/summonerInfo:
  *   get:
  *     tags:
  *       - Summoner
- *     summary: 유저 게임 리스트 가져옴
- *     description: 유저 게임 리스트 가져옴
+ *     summary: 유저 정보 가져옴
+ *     description: 유저 정보 가져옴
  *     parameters:
  *       - in: query
  *         name: summonerName
  *         required: true
  *         type: string
- *       - in: query
- *         name: type
- *         required: true
- *         type: string
  *     responses:
  *       '200':
- *         description: 유저 게임 리스트 가져오기 성공
+ *         description: 유저 정보 가져오기 성공
  *         schema:
  *           type: object
  *           properties:
  *             success:
  *               type: boolean
- *             match:
- *               // FIXME: array가 안됨
- *               type: object
+ *             data:
+ *               properties:
+ *                 accountId:
+ *                   type: string
+ *                 id:
+ *                   type: string
+ *                 name:
+ *                   type: string
+ *                 profileIconId:
+ *                   type: integer
+ *                 puuid:
+ *                   type: string
+ *                 summonerLevel:
+ *                   type: integer
+ *                 queueType:
+ *                   type: string
+ *                 tier:
+ *                   type: string
+ *                 rank:
+ *                   type: string
+ *                 leaguePoints:
+ *                   type: integer
+ *                 wins:
+ *                   type: integer
+ *                 losses:
+ *                   type: integer
  *       '403':
  *         description: api key 만료
  *         schema:
@@ -103,6 +122,180 @@ router.get("/summonerInfo", async (req: Request, res: Response) => {
   }
 });
 
+/**
+ * @swagger
+ * /api/riot/summonerMatchList:
+ *   get:
+ *     tags:
+ *       - Summoner
+ *     summary: 유저 게임 정보 가져옴
+ *     description: 유저 게임 정보 가져옴
+ *     parameters:
+ *       - in: query
+ *         name: summonerName
+ *         required: true
+ *         type: string
+ *     responses:
+ *       '200':
+ *         description: 유저 게임 정보 가져오기 성공
+ *         schema:
+ *           type: object
+ *           properties:
+ *             success:
+ *               type: boolean
+ *             data:
+ *               properties:
+ *                 jandi:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       date:
+ *                         type: string
+ *                       win:
+ *                         type: integer
+ *                       lose:
+ *                         type: integer
+ *                       count:
+ *                         type: integer
+ *                 lineWinOrLose:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       TOP:
+ *                         type: object
+ *                         properties:
+ *                           win:
+ *                             type: integer
+ *                           lose:
+ *                             type: integer
+ *                       JUNGLE:
+ *                         type: object
+ *                         properties:
+ *                           win:
+ *                             type: integer
+ *                           lose:
+ *                             type: integer
+ *                       MIDDLE:
+ *                         type: object
+ *                         properties:
+ *                           win:
+ *                             type: integer
+ *                           lose:
+ *                             type: integer
+ *                       BOTTOM:
+ *                         type: object
+ *                         properties:
+ *                           win:
+ *                             type: integer
+ *                           lose:
+ *                             type: integer
+ *                       UTILITY:
+ *                         type: object
+ *                         properties:
+ *                           win:
+ *                             type: integer
+ *                           lose:
+ *                             type: integer
+ *                 matchArr:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       gameCreation:
+ *                         type: integer
+ *                       gameId:
+ *                         type: integer
+ *                       gameEndTimestamp:
+ *                         type: integer
+ *                       gameDuration:
+ *                         type: integer
+ *                       gameMode:
+ *                         type: string
+ *                       player:
+ *                         type: object
+ *                         properties:
+ *                           summonerName:
+ *                             type: string
+ *                           championName:
+ *                             type: string
+ *                           champLevel:
+ *                             type: integer
+ *                           kills:
+ *                             type: integer
+ *                           deaths:
+ *                             type: integer
+ *                           assists:
+ *                             type: integer
+ *                           cs:
+ *                             type: integer
+ *                           items:
+ *                             type: array
+ *                             items:
+ *                               type: integer
+ *                           spells:
+ *                             type: array
+ *                             items:
+ *                               type: string
+ *                           wardsKilled:
+ *                             type: integer
+ *                           wardsPlaced:
+ *                             type: integer
+ *                           detectorWardsPlaced:
+ *                             type: integer
+ *                       enemy:
+ *                         type: object
+ *                         properties:
+ *                           championName:
+ *                             type: string
+ *                           kills:
+ *                             type: integer
+ *                           wardsKilled:
+ *                             type: integer
+ *                           wardsPlaced:
+ *                              type: integer
+ *                           detectorWardsPlaced:
+ *                              type: integer
+ *                       players:
+ *                         type: array
+ *                         items:
+ *                           type: object
+ *                           properties:
+ *                             summonerName:
+ *                               type: string
+ *                             championName:
+ *                               type: string
+ *                       win:
+ *                         type: integer
+ *                       myIndex:
+ *                         type: integer
+ *                       enemyIndex:
+ *                         type: integer
+ *                       detail:
+ *                         type: object
+ *                         nullable: true
+ *       '403':
+ *         description: api key 만료
+ *         schema:
+ *           type: object
+ *           properties:
+ *             success:
+ *               type: boolean
+ *               example: false
+ *             err:
+ *               type: object
+ *       '500':
+ *         description: 유저 게임 리스트 가져오기 실패
+ *         schema:
+ *           type: object
+ *           properties:
+ *             success:
+ *               type: boolean
+ *               example: false
+ *             err:
+ *               type: object
+ */
 router.get("/summonerMatchList", async (req: Request, res: Response) => {
   try {
     const puuid = req.query.puuid as string;
@@ -340,9 +533,56 @@ router.get("/spectatorInfo", async (req: Request, res: Response) => {
  *           properties:
  *             success:
  *               type: boolean
- *             match:
- *               // FIXME: array가 안됨
+ *             data:
  *               type: object
+ *               properties:
+ *                 gameId:
+ *                   type: integer
+ *                 player:
+ *                   type: object
+ *                   properties:
+ *                     totalCs:
+ *                       type: integer
+ *                     totalDamageDoneToChampions:
+ *                       type: integer
+ *                     totalDamageTaken:
+ *                       type: integer
+ *                     totalGold:
+ *                       type: integer
+ *                 enemy:
+ *                   type: object
+ *                   properties:
+ *                     totalCs:
+ *                       type: integer
+ *                     totalDamageDoneToChampions:
+ *                       type: integer
+ *                     totalDamageTaken:
+ *                       type: integer
+ *                     totalGold:
+ *                       type: integer
+ *                 timeLine:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       player:
+ *                         type: object
+ *                         properties:
+ *                           totalGold:
+ *                             type: integer
+ *                           totalCs:
+ *                             type: integer
+ *                           xp:
+ *                             type: integer
+ *                       enemy:
+ *                         type: object
+ *                         properties:
+ *                           totalGold:
+ *                             type: integer
+ *                           totalCs:
+ *                             type: integer
+ *                           xp:
+ *                             type: integer
  *       '403':
  *         description: api key 만료
  *         schema:

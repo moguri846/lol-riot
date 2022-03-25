dc317313c527d4b0d1c412994ec74a55c55ba95e
moguri846 <dkffkekahsl1@naver.com>
2022-03-25 20:07:48 +0900

    [FE] refactor !: SearchSummonerInputForm Omit multiSearch 추가
    SearchSummonerInputForm Omit multiSearch 추가

--- client/src/components/Molecules/SearchSummonerInputForm/SearchSummonerInputForm.tsx
+++ client/src/components/Molecules/SearchSummonerInputForm/SearchSummonerInputForm.tsx
@@ -3,7 +3,12 @@ import Input from "../../Atoms/Input/Input";
 import Button from "../../Atoms/Button/Button";
 import { IUseSearch } from "../../../hooks/useSearch";
 
-const SearchSummonerInputForm = ({ summonerName, onChange, onEnter, onClick }: Omit<IUseSearch, "onMatchDetail">) => {
+const SearchSummonerInputForm = ({
+  summonerName,
+  onChange,
+  onEnter,
+  onClick,
+}: Omit<IUseSearch, "onMatchDetail" | "multiSearch">) => {
   return (
     <>
       <Input type="text" placeholder="소환사 이름" value={summonerName} onChange={onChange} onKeyDown={onEnter} />

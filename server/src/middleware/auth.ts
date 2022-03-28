12cc4a1c69f8cf3f3eaf3cfc195d6fb5abf81bd1
moguri846 <dkffkekahsl1@naver.com>
2022-03-28 16:45:46 +0900

    [BE] feat !: authChecker middleware 추가
    authChecker middleware 추가

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import { User } from "../models/User";
+import { resFunc } from "../routes/common/ResSuccessOrFalse.function";
+
+export const authChecker = async (req: Request, res: Response, next: NextFunction) => {
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.split("Bearer ")[1];
+
+    try {
+      const user = await User.findByToken(token);
+
+      req.user = user;
+
+      next();
+    } catch (err: any) {
+      return resFunc({ res, err: { status: 401, message: "Unauthorized" } });
+    }
+  } else {
+    return resFunc({ res, err: { status: 401, message: "Unauthorized" } });
+  }
+};

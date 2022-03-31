1a8f7e0dd6f1269af6fbbe46f54a24871f879412
moguri846 <dkffkekahsl1@naver.com>
2022-03-31 23:12:54 +0900

    [FE] refactor !: oauth.ts => auth 이름 변경 + 함수 이름 변경
    oauth.ts => auth 이름 변경 + 함수 이름 변경

---
+++ client/src/API/auth.ts
@@ -0,0 +1,46 @@
+import Send from "./interceptor";
+import { Methods } from "./common/methods";
+import { IAuthLoginPrameter } from "../_actions/user/interface/auth.interface";
+import { REFRESH_TOKEN } from "../_actions/user/constant/user.constant";
+
+const signIn = ({ code, state, info, type }: IAuthLoginPrameter) => {
+  const body = {
+    code: code || null,
+    state: state || null,
+    info: info || null,
+  };
+
+  return Send({
+    method: Methods.POST,
+    url: `/auth/${type}/signIn`,
+    data: body,
+  });
+};
+
+const myInfo = (type: string) => {
+  return Send({
+    method: Methods.GET,
+    url: `/auth/${type}/myInfo`,
+  });
+};
+
+const reissueToken = (type: string) => {
+  const refresh_token = localStorage.getItem(REFRESH_TOKEN) as string;
+
+  console.log("refresh_token", refresh_token);
+
+  return Send({
+    method: Methods.POST,
+    url: `/auth/${type}/reissueToken`,
+    data: { refresh_token },
+  });
+};
+
+const logout = (type: string) => {
+  return Send({
+    method: Methods.POST,
+    url: `/auth/${type}/logout`,
+  });
+};
+
+export { signIn, reissueToken, myInfo, logout };

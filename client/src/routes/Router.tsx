88eec3384d2b1b44df8e705297d4a99d0a450ca6
moguri846 <dkffkekahsl1@naver.com>
2022-04-01 17:36:45 +0900

    [FE] feat !: Router signIn, signUp, notFound route 추가
    Router signIn, signUp, notFound route 추가

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -4,11 +4,13 @@ import React from "react";
 import IndexPage from "../pages/IndexPage/IndexPage";
 import SummonerPage from "../pages/SummonerPage/SummonerPage";
 import MultiSearchPage from "../pages/MultiSearchPage/MultiSearchPage";
-import LoginPage from "../pages/LoginPage/LoginPage";
+import LoginPage from "../pages/SignInPage/SignInPage";
 import OAuth from "../components/OAuth";
 import Auth from "../hoc/Auth/Auth";
 import PostPage from "../pages/PostPage/PostPage";
 import ArticlePage from "../pages/ArticlePage/ArticlePage";
+import SignUpPage from "../pages/SignUpPage/SignUpPage";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 
 /*
   null => 누구나 가능
@@ -22,10 +24,12 @@ const Router = () => {
       <Route path="/" element={Auth(IndexPage, null)} />
       <Route path="/summoner=:summonerName" element={Auth(SummonerPage, null)} />
       <Route path="/multi_search=:summonerName" element={Auth(MultiSearchPage, null)} />
-      <Route path="/login" element={Auth(LoginPage, false)} />
+      <Route path="/signIn" element={Auth(LoginPage, false)} />
+      <Route path="/signUp" element={Auth(SignUpPage, false)} />
       <Route path="/oauth/:type" element={Auth(OAuth, false)} />
       <Route path="/post/:type" element={Auth(PostPage, true)} />
       <Route path="/article/:id" element={Auth(ArticlePage, null)} />
+      <Route path="*" element={Auth(NotFoundPage, null)} />
     </Routes>
   );
 };

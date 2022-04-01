ced12fae3cbc703943cbe7419b26fbccfd209e88
moguri846 <dkffkekahsl1@naver.com>
2022-04-01 18:59:07 +0900

    [FE] refactor !: OAuth img 경로 수정
    OAuth img 경로 수정

--- client/src/pages/SignInPage/SignInPage.tsx
+++ client/src/pages/SignInPage/SignInPage.tsx
@@ -62,14 +62,14 @@ const SignInPage = () => {
         <a
           href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`}
         >
-          <img src="/assets/oAuthImage/kakao_login_medium_wide.png" alt="kakao_login_medium_wide" />
+          <img src="/assets/image/oAuth/kakao_login_medium_wide.png" alt="kakao_login_medium_wide" />
         </a>
         <a
           href={`https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
             process.env.REACT_APP_NAVER_CLIENT_ID
           }&redirect_uri=${encodeURI(process.env.REACT_APP_NAVER_REDIRECT_URI as string)}&state=RANDOM_STATE`}
         >
-          <img src="/assets/oAuthImage/btnG_완성형.png" alt="btnG_완성형" />
+          <img src="/assets/image/oAuth/btnG_완성형.png" alt="btnG_완성형" />
         </a>
       </S.OAuthButtons>
     </S.SignInContainer>

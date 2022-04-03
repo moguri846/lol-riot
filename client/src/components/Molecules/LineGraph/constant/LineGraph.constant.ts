b325dec64d01b5abc5782c87aead9e2440677621
moguri846 <dkffkekahsl1@naver.com>
2022-04-04 05:48:11 +0900

    [FE] refactor !: matchDetail component 리팩토링
    matchDetail component 리팩토링

--- client/src/components/Molecules/LineGraph/constant/LineGraph.constant.ts
+++
@@ -1,5 +0,0 @@
-const TOTAL_GOLD = "totalGold";
-const TOTAL_CS = "totalCs";
-const XP = "xp";
-
-export { TOTAL_GOLD, TOTAL_CS, XP };
--- client/src/components/Organisms/SummonerMatchDetail/SummonerMatchDetail.tsx
+++ client/src/components/Organisms/SummonerMatchDetail/SummonerMatchDetail.tsx
@@ -1,41 +1,50 @@
 import React, { useState } from "react";
 import { ComparingWithEnemyType } from "../../../_actions/riot/interface/matchSummary.interface";
-import { TOTAL_CS, TOTAL_GOLD, XP } from "../../Molecules/LineGraph/constant/LineGraph.constant";
 import { TimelineOptionsType } from "../../Molecules/LineGraph/interface/LineGraph.interface";
 import LineGraph from "../../Molecules/LineGraph/LineGraph";
 import ProgressBar from "../../Molecules/ProgressBar/ProgressBar";
 import { LINE_MATCH, ANALYSIS, TIMELINE } from "../SummonerMatchItem/constant/SummonerMatchItem.constant";
 import { IOptionsList, MatchDetailOptionsType } from "../SummonerMatchItem/interface/SummonerMatchItem.interface";
-
+import {
+  KILLS,
+  LEVEL,
+  TOTAL_CS,
+  TOTAL_DAMAGE_DONE_TO_CHAMPIONS,
+  TOTAL_DAMAGE_TAKEN,
+  TOTAL_GOLD,
+  WARDS_PLACED,
+  XP,
+} from "./constant/SummonerMatchDetail.constant";
+import {
+  CommonMatchDetailType,
+  LineMatchType,
+  MatchDetailAnalysisType,
+} from "./interface/SummonerMatchDetail.interface";
 import * as S from "./style";
 
 interface IProps {
   match: ComparingWithEnemyType;
 }
 
-const progressObjs: {
+const commonDetailKeys: {
   title: string;
-  key: "kills" | "totalGold" | "totalDamageDoneToChampions" | "wardsPlaced" | "totalDamageTaken" | "totalCs";
+  key: CommonMatchDetailType;
 }[] = [
-  { title: "적 처치", key: "kills" },
-  { title: "골드 획득량", key: "totalGold" },
-  { title: "가한 피해량", key: "totalDamageDoneToChampions" },
-  { title: "와드 설치", key: "wardsPlaced" },
-  { title: "받은 피해량", key: "totalDamageTaken" },
-  { title: "CS", key: "totalCs" },
+  { title: "골드 획득량", key: TOTAL_GOLD },
+  { title: "가한 피해량", key: TOTAL_DAMAGE_DONE_TO_CHAMPIONS },
+  { title: "받은 피해량", key: TOTAL_DAMAGE_TAKEN },
+  { title: "CS", key: TOTAL_CS },
 ];
+
+const analysis: {
+  title: string;
+  key: MatchDetailAnalysisType;
+}[] = [{ title: "적 처치", key: KILLS }, { title: "와드 설치", key: WARDS_PLACED }, ...commonDetailKeys];
 
 const lineMatchs: {
   title: string;
-  key: "level" | "xp" | "totalGold" | "totalDamageDoneToChampions" | "totalDamageTaken" | "totalCs";
-}[] = [
-  { title: "레벨", key: "level" },
-  { title: "경험치", key: "xp" },
-  { title: "골드 획득량", key: "totalGold" },
-  { title: "가한 피해량", key: "totalDamageDoneToChampions" },
-  { title: "받은 피해량", key: "totalDamageTaken" },
-  { title: "CS", key: "totalCs" },
-];
+  key: LineMatchType;
+}[] = [{ title: "레벨", key: LEVEL }, { title: "경험치", key: XP }, ...commonDetailKeys];
 
 const SummonerMatchDetail = ({ match }: IProps) => {
   const [matchDetailOptions, setMatchDetailOptions] = useState<MatchDetailOptionsType[]>([
@@ -129,7 +138,6 @@ const SummonerMatchDetail = ({ match }: IProps) => {
       {matchDetailSelected === LINE_MATCH && (
         <ul style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around" }}>
           {lineMatchs.map(({ title, key }) => {
-            // const isPlayerData = key === "kills" || key === "wardsPlaced";
             return (
               <li key={key}>
                 <ProgressBar
@@ -152,7 +160,7 @@ const SummonerMatchDetail = ({ match }: IProps) => {
       )}
       {matchDetailSelected === ANALYSIS && (
         <ul style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around" }}>
-          {progressObjs.map(({ title, key }) => {
+          {analysis.map(({ title, key }) => {
             const isPlayerData = key === "kills" || key === "wardsPlaced";
             return (
               <li key={key}>

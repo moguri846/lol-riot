2cadbd946d9a5a0c96cce816a6e6848d784475da
moguri846 <dkffkekahsl1@naver.com>
2022-04-08 02:07:43 +0900

    [FE] refactor !: App.tsx jsx 수정
    App.tsx jsx 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,36 +4,12 @@ import Header from "./components/Organisms/Header/Header";
 import Router from "./routes/Router";
 import GlobalStyle from "./styles/GlobalStyle";
 
-const Wrap = styled.main`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const RouterContainer = styled.div`
-  width: 760px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  @media screen and (max-width: 760px) {
-    width: 100%;
-  }
-`;
-
 const App = () => {
   return (
     <>
       <GlobalStyle />
       <Header />
-      <Wrap>
-        <RouterContainer>
-          <Router />
-        </RouterContainer>
-      </Wrap>
+      <Router />
     </>
   );
 };

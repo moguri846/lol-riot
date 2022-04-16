16e580b8e297a0438e3faa90d30f1d9b9f34103b
moguri846 <dkffkekahsl1@naver.com>
2022-04-16 23:10:50 +0900

    [FE] refactor !: router lazy, Suspense적용 => resources 줄이기
    router lazy, Suspense적용 => resources 줄이기

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 
-import React from "react";
-import IndexPage from "../pages/IndexPage/IndexPage";
-import SummonerPage from "../pages/SummonerPage/SummonerPage";
-import MultiSearchPage from "../pages/MultiSearchPage/MultiSearchPage";
-import LoginPage from "../pages/SignInPage/SignInPage";
-import OAuth from "../components/OAuth";
+import React, { Suspense } from "react";
 import Auth from "../hoc/Auth/Auth";
-import PostPage from "../pages/PostPage/PostPage";
-import ArticlePage from "../pages/ArticlePage/ArticlePage";
-import SignUpPage from "../pages/SignUpPage/SignUpPage";
-import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
+import IndexPage from "../pages/IndexPage/IndexPage";
+const SummonerPage = React.lazy(() => import("../pages/SummonerPage/SummonerPage"));
+const MultiSearchPage = React.lazy(() => import("../pages/MultiSearchPage/MultiSearchPage"));
+const SignInPage = React.lazy(() => import("../pages/SignInPage/SignInPage"));
+const OAuth = React.lazy(() => import("../components/OAuth"));
+const PostPage = React.lazy(() => import("../pages/PostPage/PostPage"));
+const ArticlePage = React.lazy(() => import("../pages/ArticlePage/ArticlePage"));
+const SignUpPage = React.lazy(() => import("../pages/SignUpPage/SignUpPage"));
+const NotFoundPage = React.lazy(() => import("../pages/NotFoundPage/NotFoundPage"));
 
 /*
   null => 누구나 가능
@@ -20,17 +20,19 @@ import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={Auth(IndexPage, null)} />
-      <Route path="/summoner=:summonerName" element={Auth(SummonerPage, null)} />
-      <Route path="/multi_search=:summonerName" element={Auth(MultiSearchPage, null)} />
-      <Route path="/signIn" element={Auth(LoginPage, false)} />
-      <Route path="/signUp" element={Auth(SignUpPage, false)} />
-      <Route path="/oauth/:type" element={Auth(OAuth, false)} />
-      <Route path="/post/:type" element={Auth(PostPage, true)} />
-      <Route path="/article/:id" element={Auth(ArticlePage, null)} />
-      <Route path="*" element={Auth(NotFoundPage, null)} />
-    </Routes>
+    <Suspense fallback={<div className="loading"></div>}>
+      <Routes>
+        <Route path="/" element={Auth(IndexPage, null)} />
+        <Route path="/summoner=:summonerName" element={Auth(SummonerPage, null)} />
+        <Route path="/multi_search=:summonerName" element={Auth(MultiSearchPage, null)} />
+        <Route path="/signIn" element={Auth(SignInPage, false)} />
+        <Route path="/signUp" element={Auth(SignUpPage, false)} />
+        <Route path="/oauth/:type" element={Auth(OAuth, false)} />
+        <Route path="/post/:type" element={Auth(PostPage, true)} />
+        <Route path="/article/:id" element={Auth(ArticlePage, null)} />
+        <Route path="*" element={Auth(NotFoundPage, null)} />
+      </Routes>
+    </Suspense>
   );
 };
 

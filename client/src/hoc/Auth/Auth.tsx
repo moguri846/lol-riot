84ef674acd5cd652d085f721563fe06ca97c0ee8
moguri846 <dkffkekahsl1@naver.com>
2022-04-18 22:53:16 +0900

    [FE] refactor !: useEffect deps isLogin 추가
    useEffect deps isLogin 추가

--- client/src/hoc/Auth/Auth.tsx
+++ client/src/hoc/Auth/Auth.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { ITokenStatus } from "../../_actions/user/interface/auth.interface";
 import { myInfoOAuth, oAuthTokenCheck } from "../../_actions/user/authActions";
 import useSnackBar from "../../hooks/useSnackBar";
+import { RootReducerType } from "../../_reducers/rootReducer";
 
 const auth = (SpecificComponent: React.FC, option: boolean | null) => {
   const AuthenticationCheck = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const { snackbar } = useSnackBar();
+    const isLogin = useSelector((state: RootReducerType) => state.user.token.isLogin);
 
     useEffect(() => {
       const check = async () => {
@@ -36,7 +38,7 @@ const auth = (SpecificComponent: React.FC, option: boolean | null) => {
         }
       };
       check();
-    }, []);
+    }, [isLogin]);
     return <SpecificComponent />;
   };
 

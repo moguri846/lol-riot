c7ab6424a258a8ded1d0e29da42b38a74acdce51
moguri846 <dkffkekahsl1@naver.com>
2022-05-10 19:08:31 +0900

    [FE] fix !: env 이름 수정
    env 이름 수정

--- client/components/Organisms/SiginInOrUp/SignInOrUp.tsx
+++ client/components/Organisms/SiginInOrUp/SignInOrUp.tsx
@@ -75,7 +75,7 @@ const SignInOrUp = ({ signIn }: IProps) => {
           </p>
           <S.OAuthButtons>
             <a
-              href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.VITE_KAKAO_REDIRECT_URI}&response_type=code`}
+              href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`}
             >
               <img src="/assets/image/oAuth/kakao_login_medium_wide.png" alt="kakao_login_medium_wide" />
             </a>

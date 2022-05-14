84cb5525c1a90fc5ae66b21077f93affbeda4df4
moguri846 <dkffkekahsl1@naver.com>
2022-05-14 13:08:06 +0900

    [FE] feat !: login 시 localStorage에 token 저장
    login 시 localStorage에 token 저장

--- client/pages/signIn.tsx
+++ client/pages/signIn.tsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { signIn } from "../API/auth";
@@ -17,6 +18,27 @@ const SignIn = () => {
   const onLogin = async (body: ISignInOrUpParameter) => {
     try {
       const { data } = await signIn(body);
+
+      const ACCESS_TOKEN = "ACCESS_TOKEN";
+      const ACCESS_TOKEN_EXPIRES_IN = "ACCESS_TOKEN_EXPIRES_IN";
+      const REFRESH_TOKEN = "REFRESH_TOKEN";
+      const REFRESH_TOKEN_EXPIRES_IN = "REFRESH_TOKEN_EXPIRES_IN";
+
+      const saveLocalStorage = (token: any) => {
+        localStorage.setItem(ACCESS_TOKEN, token.access_token);
+        localStorage.setItem(ACCESS_TOKEN_EXPIRES_IN, String(moment().add(token.expires_in, "second").valueOf()));
+        if (token.refresh_token) {
+          localStorage.setItem(REFRESH_TOKEN, token.refresh_token);
+        }
+        if (token.refresh_token_expires_in) {
+          localStorage.setItem(
+            REFRESH_TOKEN_EXPIRES_IN,
+            String(moment().add(token.refresh_token_expires_in, "second").valueOf())
+          );
+        }
+      };
+
+      saveLocalStorage(data.data);
 
       dispatch(tokenStatusUpdate({ isLogin: true, message: "유효한 토큰" }));
 

0c75a07295e96efb55b2827c34b3cbcd7a3d0803
moguri846 <dkffkekahsl1@naver.com>
2022-05-20 07:02:34 +0900

    [FE] Design !: Input padding 수정
    Input padding 수정

--- client/components/Atoms/Input/style.ts
+++ client/components/Atoms/Input/style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Input = styled.input`
   width: 100%;
   border: 1px solid black;
-  padding: 6px;
+  padding: 6px 0px 6px 6px;
   outline: none;
   background-color: ${({ disabled }) => (disabled ? "lightgray" : "white")};
 `;

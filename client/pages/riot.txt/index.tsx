d1f5d8178acb374011a0289d7061a6f659b30fa2
moguri846 <dkffkekahsl1@naver.com>
2022-05-21 01:45:18 +0900

    [FE] Chore !: riot.txt 추가
    riot.txt 추가

---
+++ client/pages/riot.txt/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <h1>
+      <a href="/assets/riot.txt" download="riot.txt">
+        riot.txt DOWNLOAD BUTTON
+      </a>
+    </h1>
+  );
+};
+
+export default index;

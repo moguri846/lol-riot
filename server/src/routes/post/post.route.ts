ec7a83cecd7aa55981107433b3bbbf845e7e38fe
moguri846 <dkffkekahsl1@naver.com>
2022-05-21 20:37:13 +0900

    [FE] Fix !: 로그인 안 해도 게시글 생성되는 문제 해결
    로그인 안 해도 게시글 생성되는 문제 해결

--- server/src/routes/post/post.route.ts
+++ server/src/routes/post/post.route.ts
@@ -4,13 +4,13 @@ import controller from "../../controllers/post/post.controller";
 
 const router = Router();
 
-router.post("/create", controller.create);
+router.post("/create", authChecker, controller.create);
 
 router.get("/getCategoryPosts", controller.getCategoryPosts);
 
 router.get("/getPost", controller.getPost);
 
-router.post("/updatePost", controller.updatePost);
+router.post("/updatePost", authChecker, controller.updatePost);
 
 router.get("/deletePost", authChecker, controller.deletePost);
 

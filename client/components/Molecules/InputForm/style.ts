9a4022c468aa3e6f70dfe1940f8ee45cf0ba015a
moguri846 <dkffkekahsl1@naver.com>
2022-05-21 22:31:31 +0900

    [FE] Chore !: InputForm 추가 + interface, style 수정
    InputForm 추가 + interface, style 수정

---
+++ client/components/Molecules/InputForm/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const InputFormContainer = styled.div`
+  width: 100%;
+  display: flex;
+
+  & > input {
+    flex: 8;
+  }
+
+  & > button {
+    flex: 1;
+  }
+`;
+
+export { InputFormContainer };

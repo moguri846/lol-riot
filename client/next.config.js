e25d8af80d82af7b498062526a93dc8fa6b6542c
moguri846 <dkffkekahsl1@naver.com>
2022-05-26 07:46:32 +0900

    [FE] Feat !: next.config /riot.txt rewrites 추가
    next.config /riot.txt rewrites 추가

--- client/next.config.js
+++ client/next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: ["ddragon.leagueoflegends.com"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/riot.txt",
+        destination: `http://localhost:5000/api/riot/riot.txt`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

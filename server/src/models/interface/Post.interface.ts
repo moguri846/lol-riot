58ff4af29812662f0f7222b5c41808fa8539939f
moguri846 <dkffkekahsl1@naver.com>
2022-06-04 21:36:32 +0900

    [BE] Chore !: IComments 추가
    IComments 추가

--- server/src/models/interface/Post.interface.ts
+++ server/src/models/interface/Post.interface.ts
@@ -1,12 +1,22 @@
 export interface IPost {
   _id: string;
-  writer: string;
+  username: string;
+  email: string;
   category: string;
   title: string;
   content: string;
   views: number;
   comments: {
-    writer: string;
+    username: string;
+    email: string;
     comment: string;
   }[];
+}
+
+export interface IComments {
+  _id?: string;
+  username: string;
+  email: string;
+  comment: string;
+  date: string;
 }

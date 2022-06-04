5f60ee7f82a64e9be8669b3b6fde0fbafbeeb588
moguri846 <dkffkekahsl1@naver.com>
2022-06-04 19:04:21 +0900

    [FE] Fix !: 여러 번 클릭 시 여러 번 요청하는 문제 해결
    여러 번 클릭 시 여러 번 요청하는 문제 해결

--- client/components/Atoms/Input/Input.tsx
+++ client/components/Atoms/Input/Input.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { IProps } from "./interface/Input.interface";
 import * as S from "./style";
 
 const Input = (props: IProps) => {
-  return <S.Input {...props} />;
+  const [timer, setTimer] = useState<any>(0);
+
+  const debounceOnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      if (timer) {
+        clearTimeout(timer);
+      }
+
+      setTimer(setTimeout(() => props.onKeyDown(e), 500));
+    }
+  };
+
+  return <S.Input {...props} onKeyDown={debounceOnKeyDown} />;
 };
 
 export default Input;

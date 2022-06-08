0ec5e9347ba48183045f053a40e3a43b1d585191
moguri846 <dkffkekahsl1@naver.com>
2022-06-08 23:52:21 +0900

    [BE] Refactor !: TokenExp 삭제
    TokenExp 삭제

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -72,18 +72,14 @@ userSchema.methods.generateToken = function (): Promise<IGenerateToken_R> {
   const user = this;
 
   const accessToken = jwt.sign({}, jwtSecretConfig.jwtSecret, { expiresIn: "30m" });
-  const accessTokenExp = getTimeToSec(30, "minute");
   const refreshToken = jwt.sign({ id: user._id }, jwtSecretConfig.jwtSecret, { expiresIn: "60d" });
-  const refreshTokenExp = getTimeToSec(60, "day");
 
   user.access_token = accessToken;
   user.refresh_token = refreshToken;
 
   const token: IUserToken = {
     access_token: accessToken,
-    expires_in: accessTokenExp,
     refresh_token: refreshToken,
-    refresh_token_expires_in: refreshTokenExp,
   };
 
   return user
@@ -94,11 +90,9 @@ userSchema.methods.generateToken = function (): Promise<IGenerateToken_R> {
 
 userSchema.statics.reissueToken = function (refresh_token: string): Promise<string | IUserToken> {
   const accessToken = jwt.sign({}, jwtSecretConfig.jwtSecret, { expiresIn: "30m" });
-  const accessTokenExp = getTimeToSec(30, "minute");
 
   let token: IUserToken = {
     access_token: accessToken,
-    expires_in: accessTokenExp,
   };
 
   return new Promise(async (resolve, reject) => {
@@ -119,11 +113,9 @@ userSchema.statics.reissueToken = function (refresh_token: string): Promise<stri
 
         if (exp - now <= getTimeToSec(30, "day")) {
           const refreshToken = jwt.sign({ id: user._id }, jwtSecretConfig.jwtSecret, { expiresIn: "60d" });
-          const refreshTokenExp = getTimeToSec(60, "day");
           token = {
             ...token,
             refresh_token: refreshToken,
-            refresh_token_expires_in: refreshTokenExp,
           };
 
           user.refresh_token = refreshToken;

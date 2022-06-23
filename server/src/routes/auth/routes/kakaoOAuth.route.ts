d1ee505514dc615083a6af46eac8d7c74ae6ed5a
moguri846 <dkffkekahsl1@naver.com>
2022-06-23 16:12:44 +0900

    [BE] Feat !: kakao.controller middleware 추가
    kakao.controller middleware 추가

--- server/src/routes/auth/routes/kakaoOAuth.route.ts
+++ server/src/routes/auth/routes/kakaoOAuth.route.ts
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import controller from "../../../controllers/auth/kakao.controller";
+import { authChecker } from "../../../middleware/auth";
 
 const router = Router();
 
 router.post("/signIn", controller.signIn);
 
-router.get("/checkToken", controller.checkToken);
+router.get("/checkToken", authChecker, controller.checkToken);
 
-router.get("/myInfo", controller.myInfo);
+router.get("/myInfo", authChecker, controller.myInfo);
 
-router.post("/reissueToken", controller.reissueToken);
+router.post("/reissueToken", authChecker, controller.reissueToken);
 
-router.post("/logout", controller.logout);
+router.post("/logout", authChecker, controller.logout);
 
 export default router;

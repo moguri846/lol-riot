83e0f63719cb138a1dabce138c9830544a902daf
moguri846 <dkffkekahsl1@naver.com>
2022-06-26 13:46:53 +0900

    [FE] Refactor !: user action authType 타입 수정
    user action authType 타입 수정

--- client/toolkit/user/infoSlice/func/infoSlice.func.ts
+++ client/toolkit/user/infoSlice/func/infoSlice.func.ts
@@ -1,20 +1,18 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { myInfo } from "../../../../API/auth";
+import { AuthTypes } from "../../../../API/interface/auth.interface";
 import { INFO, USER } from "../../constant/user.constant";
 
-const myInfoAction = createAsyncThunk(
-  `${USER}/${INFO}`,
-  async (type: "searchMyName" | "kakao", { rejectWithValue }) => {
-    try {
-      const {
-        data: { data: info },
-      } = await myInfo(type);
+const myInfoAction = createAsyncThunk(`${USER}/${INFO}`, async (type: AuthTypes, { rejectWithValue }) => {
+  try {
+    const {
+      data: { data: info },
+    } = await myInfo(type);
 
-      return info;
-    } catch (err) {
-      return rejectWithValue(err.response.data);
-    }
+    return info;
+  } catch (err) {
+    return rejectWithValue(err.response.data);
   }
-);
+});
 
 export { myInfoAction };

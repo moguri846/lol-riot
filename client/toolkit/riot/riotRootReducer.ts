a8ded3a588a2b6b01898cd1035ed3781cd68aec7
moguri846 <dkffkekahsl1@naver.com>
2022-06-28 07:17:03 +0900

    [FE] Remove !: rootReducer multiSearch, spectator 삭제
    rootReducer multiSearch, spectator 삭제

--- client/toolkit/riot/riotRootReducer.ts
+++ client/toolkit/riot/riotRootReducer.ts
@@ -1,14 +1,10 @@
 import { combineReducers } from "redux";
 import summonerInfo from "./summonerInfoSlice/summonerInfoSlice";
-import spectator from "./spectatorSlice/spectatorSlice";
 import gameInfo from "./gameInfoSlice/gameInfoSlice";
-import multiSearch from "./multiSearchSlice/multiSearchSlice";
 
 const riotRootReducer = combineReducers({
   summonerInfo,
-  spectator,
   gameInfo,
-  multiSearch,
 });
 
 export default riotRootReducer;

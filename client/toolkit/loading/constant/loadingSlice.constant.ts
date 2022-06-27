35db1f708f2b4c0f762148a886de2fca977072ab
moguri846 <dkffkekahsl1@naver.com>
2022-06-28 03:07:28 +0900

    [FE] Chore !: LoadingTypes constant, type 추가
    LoadingTypes constant, type 추가

--- client/toolkit/loading/constant/loadingSlice.constant.ts
+++ client/toolkit/loading/constant/loadingSlice.constant.ts
@@ -1,4 +1,9 @@
 const LOADING = "LOADING";
 const FULFILLED = "FULFILLED";
 
-export { LOADING, FULFILLED };
+const SUMMONER_INFO = "summonerInfo";
+const SPECTATOR = "spectator";
+const GAME_INFO = "gameInfo";
+const MULTI_SEARCH = "multiSearch";
+
+export { LOADING, FULFILLED, SUMMONER_INFO, SPECTATOR, GAME_INFO, MULTI_SEARCH };

9a65235d3f71e768afa7920330d282d3341907ee
moguri846 <dkffkekahsl1@naver.com>
2022-06-28 02:12:01 +0900

    [FE] Refactor !: [name].tsx 리팩토링
    [name].tsx 리팩토링

--- client/pages/summoner/[name].tsx
+++ client/pages/summoner/[name].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Spectator from "../../components/Organisms/Spectator/Spectator";
 import SummonerInfo from "../../components/Organisms/SummonerInfo/SummonerInfo";
 import Seo from "../../components/Seo/Seo";
@@ -7,15 +7,10 @@ import useSnackBar from "../../hooks/useSnackBar";
 import { loadingAction, selectLoading } from "../../toolkit/loading/loadingSlice";
 import { useAppDispatch, useAppSelector } from "../../hooks/useRedux";
 import { selectGameInfo } from "../../toolkit/riot/gameInfoSlice/gameInfoSlice";
-import { gameInfoAction } from "../../toolkit/riot/gameInfoSlice/func/gameInfoSlice.func";
 import { spectatorInfo } from "../../toolkit/riot/spectatorSlice/func/spectatorSlice.func";
 import { selectSpectator } from "../../toolkit/riot/spectatorSlice/spectatorSlice";
-import { summonerInfoAction } from "../../toolkit/riot/summonerInfoSlice/func/summonerSlice.func";
-import { selectSummonerInfo } from "../../toolkit/riot/summonerInfoSlice/summonerInfoSlice";
 import GameInfo from "../../components/Organisms/GameInfo/GameInfo";
 import WithAuth from "../../hoc";
-import { PayloadAction } from "@reduxjs/toolkit";
-import { ISummonerSuccess } from "../../toolkit/riot/summonerInfoSlice/interface/summonerInfoSlice.interface";
 
 interface IProps {
   summonerName: string;
@@ -25,34 +20,11 @@ const Summoner = ({ summonerName }: IProps) => {
   const router = useRouter();
   const dispatch = useAppDispatch();
 
-  const loading = useAppSelector(selectLoading);
-  const summonerInfo = useAppSelector(selectSummonerInfo);
-  const spectator = useAppSelector(selectSpectator);
-  const gameInfo = useAppSelector(selectGameInfo);
-
-  useEffect(() => {
-    (async () => {
-      dispatch(loadingAction({ summonerInfo: true, gameInfo: true }));
-
-      const {
-        payload: { success, puuid },
-      } = (await dispatch(summonerInfoAction(summonerName))) as PayloadAction<ISummonerSuccess, string>;
-
-      dispatch(loadingAction({ summonerInfo: false }));
-
-      if (success === undefined) {
-        await dispatch(gameInfoAction(puuid));
-      }
-
-      dispatch(loadingAction({ gameInfo: false }));
-    })();
-  }, [summonerName]);
-
   const { snackbar } = useSnackBar();
 
   const [spectatorToggle, setSpectatorToggle] = useState(false);
 
-  const handleSpectatorToggle = async () => {
+  const handleSpectatorToggle = async (id: string) => {
     setSpectatorToggle(!spectatorToggle);
 
     if (spectatorToggle) {
@@ -62,7 +34,7 @@ const Summoner = ({ summonerName }: IProps) => {
     try {
       dispatch(loadingAction({ spectator: true }));
 
-      await dispatch(spectatorInfo(summonerInfo.id));
+      await dispatch(spectatorInfo(id));
 
       dispatch(loadingAction({ spectator: false }));
     } catch (err: any) {
@@ -81,21 +53,18 @@ const Summoner = ({ summonerName }: IProps) => {
         socialUrl={`${router.asPath}`}
       />
       <SummonerInfo
-        loading={loading.summonerInfo}
-        summoner={summonerInfo}
+        summonerName={summonerName}
         spectatorToggle={spectatorToggle}
         onSpectatorToggle={handleSpectatorToggle}
         searchSummoner
       />
-      {summonerInfo.success === undefined && (
-        <>
-          {spectatorToggle ? (
-            <Spectator loading={loading.spectator} spectator={spectator} summonerName={summonerName} />
-          ) : (
-            <GameInfo loading={loading.gameInfo} gameInfo={gameInfo} />
-          )}
-        </>
-      )}
+      <>
+        {spectatorToggle ? (
+          <Spectator loading={loading.spectator} spectator={spectator} summonerName={summonerName} />
+        ) : (
+          <GameInfo loading={loading.gameInfo} gameInfo={gameInfo} />
+        )}
+      </>
     </>
   );
 };

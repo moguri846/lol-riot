766e360f278df2c066ba92f195b7844a51b40fe5
moguri846 <dkffkekahsl1@naver.com>
2022-06-28 07:07:05 +0900

    [FE] Refactor !: MutiSearch useGetData hook 추가
    MutiSearch useGetData hook 추가

--- client/components/Organisms/MultiSearch/MultiSearch.tsx
+++ client/components/Organisms/MultiSearch/MultiSearch.tsx
@@ -1,65 +1,76 @@
 import React from "react";
 import moment from "moment";
-import SummonerInfo from "../SummonerInfo/SummonerInfo";
-import { MultiSearchType } from "../../../toolkit/riot/multiSearchSlice/interface/multiSearchSlice.interface";
-import * as S from "./style";
+import { IMultiSearch } from "../../../toolkit/riot/multiSearchSlice/interface/multiSearchSlice.interface";
 import ErrorForm from "../../Molecules/ErrorForm/ErrorForm";
 import { getDataDragonImg } from "../../common/func/common.func";
 import Image from "next/image";
 import MultiSearchSkeleton from "./MultiSearchSkeleton";
+import useGetData from "../../../hooks/useGetData";
+import { loadMultiSearch } from "../../../API/riot";
+import MultiSearchSummonerInfo from "../MultiSearchSummonerInfo/MultiSearchSummonerInfo";
+import * as S from "./style";
 
 interface IProps {
-  loading: boolean;
-  multiSearch: MultiSearchType;
+  summonerNames: string[];
 }
 
-const MultiSearch = ({ loading, multiSearch }: IProps) => {
+const MultiSearch = ({ summonerNames }: IProps) => {
+  const {
+    loading,
+    data: { data: multiSearch },
+    error,
+  } = useGetData<IMultiSearch[]>({
+    loadingType: "multiSearch",
+    cb: () => loadMultiSearch(summonerNames),
+    deps: "",
+  });
+
   return (
     <>
       {loading ? (
-        <>
-          <MultiSearchSkeleton />
-        </>
+        <MultiSearchSkeleton />
       ) : (
         <>
-          {multiSearch.success === false ? (
-            <ErrorForm message={multiSearch.data} {...multiSearch} message404="유저 정보를 불러오지 못했습니다." />
+          {error.isError ? (
+            <ErrorForm message={error.message} {...error} message404="유저 정보를 불러오지 못했습니다." />
           ) : (
-            <S.SummonerList>
-              {multiSearch.data.map(({ summonerInfo, mostLine, matchArr }) => (
-                <S.SummonerItem key={summonerInfo.puuid}>
-                  <SummonerInfo loading={loading} summoner={summonerInfo} multiSearch />
-                  <div className="game-info">
-                    <div className="most-line">
-                      <Image width={30} height={30} src={`/assets/image/line/${mostLine}.png`} alt={mostLine} />
+            <>
+              {multiSearch.map(({ summonerInfo, mostLine, matchArr }) => (
+                <React.Fragment key={summonerInfo.puuid}>
+                  <S.SummonerItem>
+                    <MultiSearchSummonerInfo summoner={summonerInfo} />
+                    <div className="game-info">
+                      <div className="most-line">
+                        <Image width={30} height={30} src={`/assets/image/line/${mostLine}.png`} alt={mostLine} />
+                      </div>
+                      <S.MatchSummaryList>
+                        {matchArr.map((match) => (
+                          <S.MatchSummaryItem className={`${match.win ? "win" : "lose"}`} key={match.gameCreation}>
+                            <div className="champion">
+                              {getDataDragonImg({ width: 30, height: 30, key: "champion", value: match.championName })}
+                            </div>
+                            <div className="line">
+                              <Image
+                                width={30}
+                                height={30}
+                                src={`/assets/image/line/${match.individualPosition}.png`}
+                                alt={match.individualPosition}
+                              />
+                            </div>
+                            <div className="kda">
+                              <span className="kills">{match.kills}</span> /{" "}
+                              <span className="deaths">{match.deaths}</span> /
+                              <span className="assists"> {match.assists}</span>
+                            </div>
+                            <div className="time-stamp">{moment(match.gameCreation).startOf("minute").fromNow()}</div>
+                          </S.MatchSummaryItem>
+                        ))}
+                      </S.MatchSummaryList>
                     </div>
-                    <S.MatchSummaryList>
-                      {matchArr.map((match) => (
-                        <S.MatchSummaryItem className={`${match.win ? "win" : "lose"}`} key={match.gameCreation}>
-                          <div className="champion">
-                            {getDataDragonImg({ width: 30, height: 30, key: "champion", value: match.championName })}
-                          </div>
-                          <div className="line">
-                            <Image
-                              width={30}
-                              height={30}
-                              src={`/assets/image/line/${match.individualPosition}.png`}
-                              alt={match.individualPosition}
-                            />
-                          </div>
-                          <div className="kda">
-                            <span className="kills">{match.kills}</span> /{" "}
-                            <span className="deaths">{match.deaths}</span> /
-                            <span className="assists"> {match.assists}</span>
-                          </div>
-                          <div className="time-stamp">{moment(match.gameCreation).startOf("minute").fromNow()}</div>
-                        </S.MatchSummaryItem>
-                      ))}
-                    </S.MatchSummaryList>
-                  </div>
-                </S.SummonerItem>
+                  </S.SummonerItem>
+                </React.Fragment>
               ))}
-            </S.SummonerList>
+            </>
           )}
         </>
       )}

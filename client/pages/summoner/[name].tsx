4f0292f60b48a50d7d0ad2fa3c7c6bcbf394ed6a
moguri846 <dkffkekahsl1@naver.com>
2022-06-28 23:11:34 +0900

    [FE] Feat !: [name].tsx 유저 정보, 게임 요청 useEffect 추가
    [name].tsx 유저 정보, 게임 요청 useEffect 추가

--- client/pages/summoner/[name].tsx
+++ client/pages/summoner/[name].tsx
@@ -1,29 +1,51 @@
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Spectator from "../../components/Organisms/Spectator/Spectator";
 import SummonerInfo from "../../components/Organisms/SummonerInfo/SummonerInfo";
 import Seo from "../../components/Seo/Seo";
 import GameInfo from "../../components/Organisms/GameInfo/GameInfo";
 import WithAuth from "../../hoc";
+import { useAppDispatch, useAppSelector } from "../../hooks/useRedux";
+import { loadingAction, selectLoading } from "../../toolkit/loading/loadingSlice";
+import { gameInfoAction } from "../../toolkit/riot/gameInfoSlice/func/gameInfoSlice.func";
+import { summonerInfoAction } from "../../toolkit/riot/summonerInfoSlice/func/summonerSlice.func";
+import { ISummoner } from "../../toolkit/riot/summonerInfoSlice/interface/summonerInfoSlice.interface";
 
 interface IProps {
   summonerName: string;
 }
 
 const Summoner = ({ summonerName }: IProps) => {
-  const [id, setId] = useState("");
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    (async () => {
+      dispatch(loadingAction({ summonerInfo: true, gameInfo: true }));
+
+      const {
+        payload: { puuid },
+      } = (await dispatch(summonerInfoAction(summonerName))) as { payload: ISummoner };
+
+      dispatch(loadingAction({ summonerInfo: false }));
+
+      if (puuid) {
+        await dispatch(gameInfoAction(puuid));
+      }
+      dispatch(loadingAction({ gameInfo: false }));
+    })();
+  }, [summonerName]);
+
   const router = useRouter();
+
+  const { summonerInfo, gameInfo } = useAppSelector(selectLoading);
 
   const [spectatorToggle, setSpectatorToggle] = useState(false);
 
-  const handleSpectatorToggle = async (id: string) => {
+  const handleSpectatorToggle = () => {
     setSpectatorToggle(!spectatorToggle);
 
     if (spectatorToggle) {
       return;
     }
-
-    setId(id);
   };
 
   return (
@@ -35,11 +57,11 @@ const Summoner = ({ summonerName }: IProps) => {
         socialUrl={`${router.asPath}`}
       />
       <SummonerInfo
-        summonerName={summonerName}
+        loading={summonerInfo}
         spectatorToggle={spectatorToggle}
         onSpectatorToggle={handleSpectatorToggle}
       />
-      <>{spectatorToggle ? <Spectator id={id} summonerName={summonerName} /> : <GameInfo />}</>
+      <>{spectatorToggle ? <Spectator summonerName={summonerName} /> : <GameInfo loading={gameInfo} />}</>
     </>
   );
 };

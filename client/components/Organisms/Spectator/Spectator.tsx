20266c81b5ddc4255a3d9834af0c0db719a159b7
moguri846 <dkffkekahsl1@naver.com>
2022-06-28 23:10:36 +0900

    [FE] Refactor !: Spectator component 리팩토링
    Spectator component 리팩토링

--- client/components/Organisms/Spectator/Spectator.tsx
+++ client/components/Organisms/Spectator/Spectator.tsx
@@ -2,24 +2,27 @@ import React from "react";
 import Skeleton from "react-loading-skeleton";
 import { loadSpectatorInfo } from "../../../API/riot";
 import useGetData from "../../../hooks/useGetData";
-import { ISpectator } from "../../../toolkit/riot/spectatorSlice/interface/spectatorSlice.interface";
+import { useAppSelector } from "../../../hooks/useRedux";
+import { selectSummonerInfo } from "../../../toolkit/riot/summonerInfoSlice/summonerInfoSlice";
 import { getDataDragonImg } from "../../common/func/common.func";
 import ErrorForm from "../../Molecules/ErrorForm/ErrorForm";
+import { ISpectator } from "./interface/Spectator.interface";
 import * as S from "./style";
 
 interface IProps {
-  id: string;
   summonerName: string;
 }
 
-const Spectator = ({ id, summonerName }: IProps) => {
+const Spectator = ({ summonerName }: IProps) => {
+  const summoner = useAppSelector(selectSummonerInfo);
+
   const {
     loading,
     data: { data: spectator },
     error,
   } = useGetData<ISpectator>({
     loadingType: "spectator",
-    cb: () => loadSpectatorInfo(id),
+    cb: () => loadSpectatorInfo(summoner.id),
     deps: "",
   });
 
@@ -35,48 +38,44 @@ const Spectator = ({ id, summonerName }: IProps) => {
                 <ErrorForm message={error.message} {...error} message404={`${summonerName}님은 게임 중이 아닙니다.`} />
               ) : (
                 <>
-                  <>
-                    <S.SpectatorPlayerList>
-                      {spectator?.players.map((player) => (
-                        <React.Fragment key={player.summonerName}>
-                          <S.SpectatorPlayer className={player.teamId === 100 ? "blue" : "red"}>
-                            <span className="name">
-                              <a href={`/summoner=${player.summonerName}`}>{player.summonerName}</a>
-                            </span>
-                            <S.ChampionStatus>
-                              <ul className="spells">
-                                {player.spells.map((spell) => (
-                                  <li key={spell}>
-                                    {getDataDragonImg({ width: 12, height: 12, key: "spell", value: spell })}
-                                  </li>
-                                ))}
-                              </ul>
-                              <div className="champion-img">
-                                {getDataDragonImg({
-                                  width: 26,
-                                  height: 26,
-                                  key: "champion",
-                                  value: player.championName,
-                                })}
-                              </div>
-                            </S.ChampionStatus>
-                          </S.SpectatorPlayer>
-                        </React.Fragment>
-                      ))}
-                    </S.SpectatorPlayerList>
-                    <S.BannedChampionList>
-                      {spectator?.bannedChampions.map((bannedChampion, i) => (
-                        <S.BannedChampion key={i}>
-                          {getDataDragonImg({
-                            width: 30,
-                            height: 30,
-                            key: "champion",
-                            value: bannedChampion.championName,
-                          })}
-                        </S.BannedChampion>
-                      ))}
-                    </S.BannedChampionList>
-                  </>
+                  <S.SpectatorPlayerList>
+                    {spectator?.players.map((player) => (
+                      <S.SpectatorPlayer key={player.summonerName} className={player.teamId === 100 ? "blue" : "red"}>
+                        <span className="name">
+                          <a href={`/summoner=${player.summonerName}`}>{player.summonerName}</a>
+                        </span>
+                        <S.ChampionStatus>
+                          <ul className="spells">
+                            {player.spells.map((spell) => (
+                              <li key={spell}>
+                                {getDataDragonImg({ width: 12, height: 12, key: "spell", value: spell })}
+                              </li>
+                            ))}
+                          </ul>
+                          <div className="champion-img">
+                            {getDataDragonImg({
+                              width: 26,
+                              height: 26,
+                              key: "champion",
+                              value: player.championName,
+                            })}
+                          </div>
+                        </S.ChampionStatus>
+                      </S.SpectatorPlayer>
+                    ))}
+                  </S.SpectatorPlayerList>
+                  <S.BannedChampionList>
+                    {spectator?.bannedChampions.map((bannedChampion, i) => (
+                      <S.BannedChampion key={i}>
+                        {getDataDragonImg({
+                          width: 30,
+                          height: 30,
+                          key: "champion",
+                          value: bannedChampion.championName,
+                        })}
+                      </S.BannedChampion>
+                    ))}
+                  </S.BannedChampionList>
                 </>
               )}
             </>

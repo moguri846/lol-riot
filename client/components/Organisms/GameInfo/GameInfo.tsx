5cf3d1f65500352a2d63f8b6f4776d4e91c011bc
moguri846 <dkffkekahsl1@naver.com>
2022-06-28 23:10:07 +0900

    [FE] Refactor !: GameInfo component 리팩토링
    GameInfo component 리팩토링

--- client/components/Organisms/GameInfo/GameInfo.tsx
+++ client/components/Organisms/GameInfo/GameInfo.tsx
@@ -1,25 +1,37 @@
-import React from "react";
-import { GameInfoType } from "../../../toolkit/riot/gameInfoSlice/interface/gameInfo.interface";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../hooks/useRedux";
+import { loadingAction } from "../../../toolkit/loading/loadingSlice";
+import { isError } from "../../../toolkit/riot/common/func/common.func";
+import { gameInfoAction } from "../../../toolkit/riot/gameInfoSlice/func/gameInfoSlice.func";
+import { selectGameInfo } from "../../../toolkit/riot/gameInfoSlice/gameInfoSlice";
+import {} from "../../../toolkit/riot/gameInfoSlice/interface/gameInfo.interface";
+import { selectSummonerInfo } from "../../../toolkit/riot/summonerInfoSlice/summonerInfoSlice";
 import ErrorForm from "../../Molecules/ErrorForm/ErrorForm";
 import SummonerGameAnalysis from "../SummonerGameAnalysis/SummonerGameAnalysis";
 import SummonerMatchList from "../SummonerMatchList/SummonerMatchList";
 
 interface IProps {
   loading: boolean;
-  gameInfo: GameInfoType;
 }
 
-const GameInfo = ({ loading, gameInfo }: IProps) => {
+const GameInfo = ({ loading }: IProps) => {
+  const summoner = useAppSelector(selectSummonerInfo);
+  const gameInfo = useAppSelector(selectGameInfo);
+
   return (
     <>
-      {gameInfo.success === false ? (
-        <>
-          <ErrorForm message={gameInfo.data} {...gameInfo} message404="게임 정보를 불러오지 못했습니다." />
-        </>
+      {isError(summoner) ? (
+        <></>
       ) : (
         <>
-          <SummonerGameAnalysis loading={loading} jandi={gameInfo.jandi} lineWinOrLose={gameInfo.lineWinOrLose} />
-          <SummonerMatchList loading={loading} matchArr={gameInfo.matchArr} />
+          {isError(gameInfo) ? (
+            <ErrorForm {...gameInfo} message404="게임 정보를 불러오지 못했습니다." />
+          ) : (
+            <>
+              <SummonerGameAnalysis loading={loading} jandi={gameInfo.jandi} lineWinOrLose={gameInfo.lineWinOrLose} />
+              <SummonerMatchList loading={loading} matchArr={gameInfo.matchArr} />
+            </>
+          )}
         </>
       )}
     </>
